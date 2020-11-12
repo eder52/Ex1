885107f5ac11334fb9b989d73f941b9610e350ea
eder52 <eder.91@web.de>
2020-11-12 10:21:40 +0100

    added "C"

--- co2.R
+++ co2.R
@@ -14,6 +14,7 @@
 #' @param gdp
 #' @param enInt
 #' @param carbInt
+#' @param output_type "C" or "CO2"
 #'
 #' @return number returns "-1" when input is flawed
 #'
@@ -21,13 +22,19 @@
 #' 6 = 1 * 2 * 3 * 1
 #'
 #' @export
-carbondioxide <- function(pop, gdp, enInt, carbInt) {
+carbondioxide <- function(pop, gdp, enInt, carbInt, output_type) {
   
   result = -1
+  
+  if(missing(output_type)) {
+    output_type = "CO2"
+  } 
+  
 
   #if (testInt(pop, lower = 1)) {
    if (pop >= 0) {
     result = pop * gdp * enInt * carbInt
+    if (output_type == "C") {result = result / 3.67}
     print(result)
     }
   
--- testing.R
+++ testing.R
@@ -6,4 +6,5 @@ test_that("testing the function", {
   expect_equal(carbondioxide(1,1,1,1), 1)
   expect_equal(carbondioxide(-1,2,3,1.2), -1)
   expect_equal(carbondioxide(2,2,2,2), 16)
+  expect_equal(carbondioxide(2,2,2,2, "C"), 16/3.67)
 })
