526f8cb92c1de655fc1b6f22970a322e565d4a1d
eder52 <eder.91@web.de>
2020-11-12 09:29:43 +0100

    inits

---
+++ co2.R
@@ -0,0 +1,25 @@
+
+
+#' Calculates Carbondioxide
+#'
+#' Simply multiplies the arguments
+#'
+#' @param pop
+#' @param gdp
+#' @param enInt
+#' @param carbInt
+#'
+#' @return number
+#'
+#' @examples
+#' 6 = 1 * 2 * 3 * 1
+#'
+#' @export
+carbondioxide <- function(pop, gdp, enInt, carbInt) {
+  return(pop * gdp * enInt * carbInt)
+}
+
+
+
+
+print(carbondioxide(1,2,3,1.2))
